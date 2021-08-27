b4370cdc7a76f59975aba41b17f0e0e1fae9caaf
Zeus1347 <74809793+zeus1347@users.noreply.github.com>
2021-08-27 15:57:18 +0000

    add more links

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ f_gifs = [
     "https://tenor.com/view/f-letter-f-burts-rip-pipec-f-throw-gif-17537831",
     "https://tenor.com/view/salute-letter-f-respect-crying-gif-17677784",
     "https://tenor.com/view/press-f-pay-respect-keyboard-gif-12855017",
+    "https://tenor.com/view/pay-respects-press-f-call-of-duty-respect-press-x-gif-22309724","https://tenor.com/view/efemann-efe-ff-f-in-the-chat-f-in-chat-gif-17919585"
 ]
-salam=["salamalaikum","salam","assalam alaikum","assalam","assalamualaikum","assalamualikum"]
-salam_r=["Assalamualikum Kenobi Al Habibi","Walaikum Assalam Al Obi Wan Al Kenobi"]
+salam_i=["salamalaikum","salam","assalam alaikum","assalam","assalamualaikum","assalamualikum","salamalekum","salamualaikum","salamualekum","assalamalekum"]
+salam_o=["Walaikum Salam Kenobi Al Habibi","Walaikum Assalam Al Obi Wan Al Kenobi"]
 
 
 @client.event
@@ -25,6 +26,8 @@ async def on_ready():
 async def on_message(msg):
     if msg.author == client.user:
         return
+    if msg.author.bot: 
+        return 
 
     if msg.content.lower() == "hello there":
         await msg.channel.send("https://tenor.com/view/hello-there-general-kenobi-star-wars-grevious-gif-17774326")
@@ -32,8 +35,8 @@ async def on_message(msg):
     if msg.content.lower() == 'f':
         await msg.channel.send(random.choice(f_gifs))
 
-    if msg.content.lower() in salam:
-        await msg.channel.send(random.choice(salam_r))
+    if msg.content.lower() in salam_i:
+        await msg.channel.send(random.choice(salam_o))
 
 
 
