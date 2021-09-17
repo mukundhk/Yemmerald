18d093debfd2ece1b490e376ec020db920270cd3
Mukund Harikumar <74809793+zeus1347@users.noreply.github.com>
2021-09-17 17:30:36 +0000

    removed db['units_dict']

--- main.py
+++ main.py
@@ -5,57 +5,7 @@ from discord.ext import commands
 from keep_alive import keep_alive
 from replit import db
 
-lst1 = [
-    [2070000, 'a', 'u', "would need about", 'c', "human hairs to lift."],
-    [
-        3.359424866, 'a', 'u', "is about the weight of", 'c',
-        "'Velener Mini Potted Plastic Fake Green Plants'."
-    ],
-    [
-        2.873563218, 'a', 'u', "is about the weight of", 'c',
-        "Minecraft: Redstone Handbook: An Official Mojang Book (Hardcover)."
-    ],
-    [
-        1.218023533, 'a', 'u', "is about the same weight as", 'c',
-        "'Double sided 60 inch Mermaker Pepperoni Pizza Blankets'."
-    ],
-    [
-        8.86280467, 'a', 'u', "is about the weight of", 'c',
-        "'6pack TWOHANDS Assorted Pastel Color Highlighters'."
-    ],
-    [
-        19471.42, 'a', 'u', "of vegan poop being burned provides about", 'c',
-        "BTU(British Thermal Unit)."
-    ],
-    [
-        361.8730093, 'a', 'u', "is about the weight of $", 'c',
-        "worth of Premium Glass Nail Files."
-    ],
-    [
-        24.32686944, 'a', 'u', "is the weight of about", 'c',
-        "'Kingston 120GB Q500 SATA3 2.5 Solid State Drives'."
-    ],
-    [
-        1.851851852, 'a', 'u',
-        "of double AA batteries could start a medium sized car about", 'c',
-        "times."
-    ]
-]
-lst2=[[39.37007874, 'a', 'u', 'is the length of approximately', 'c', "'Wooden Rice Paddle Versatile Serving Spoons' laid lengthwise."],
-[2.021, 'a', 'u', 'is about the same as', 'c', "'Logitech Wireless Keyboard K350s' laid widthwise by each other."]
-, [9.842519685, 'a', 'u', 'is about the length of', 'c', "'Bug Bite Thing Suction Tool - Poison Remover For Bug Bites's stacked on top of each other."]
-, [4.921259843, 'a', 'u', 'is about the length of', 'c', "'EuroGraphics Knittin Kittens 500-Piece Puzzle's next to each other."]
-, [2.04095794402, 'a', 'u', 'is the length of about', 'c', "'Ford F-150 Custom Fit Front FloorLiners' lined up next to each other."]
-, [9.803921569, 'a', 'u', 'is the length of about', 'c', "'Standard Diatonic Key of C, Blues Silver grey Harmonicas' lined up next to each other."]
-, [3.402772579, 'a', 'u', 'is about', 'c', 'RTX 3090 graphics cards lined up.']
-, [0.4921259843, 'a', 'u', 'is about the same as', 'c', 'times the average door frame height']
-, [1814.8820326, 'a', 'u', 'is about the same as', 'c', 'sperm cells lined up head to tail']
-, [0.5734063318, 'a', 'u', 'is the height of', 'c', "'Samsung Side by Side; Fingerprint Resistant Stainless Steel Refrigerators' stacked on top of each other."]
-, [0.00490677134, 'a', 'u', 'is about', 'c', "times the Guinness World Record for 'Longest Hot Dog'"]
-, [4.525296407, 'a', 'u', 'is the length of like', 'c', "'Zulay Premium Quality Metal Lemon Squeezers' laid next to each other."],
-[1.789549034, 'a', 'u', 'is about the same distance as', 'c', "'Replica Bilbo's from The Lord of the Rings Sting Swords'"]]
-db['length']=lst2
-db['weight'] = lst1
+del db['units_dict']
 
 client = discord.Client()
 token = os.environ['token']
