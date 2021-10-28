7dff7989b33ea4e5fd2774558a908cae2136196f
Mukund Harikumar <74809793+zeus1347@users.noreply.github.com>
2021-10-28 17:10:19 +0000

    basic code

--- main.py
+++ main.py
@@ -40,21 +40,32 @@ async def help(ctx):
   embedVar = discord.Embed(title="Yemmerald v2.3.2",description="Bot made by **@Zeus_1347#0765**. Please DM if the bot is offline or if you have any suggestions or feedback. \n ",color=0x26a43b)
   embedVar.set_thumbnail(url="https://i.imgur.com/iJn5Kgn.png")
   embedVar.add_field(name="Useless Unit Convertor",value="Use `y.convert <amount> <unit>` \nConverts your lengths, weights and time to random useless units",inline=False)
-  embedVar.add_field(name="Spam",value="Use `y.spam <user> <message> <number> \nSpams a mentioned user the specified number of times. \n<number> is an optional parameter")
-  embedVar.add_field(name="Spam DM",value="Use `y.spamdm <user> <message> <number> \nSpams a mentioned user in their DM the specified number of times. Use this when you dont want to spam on the server \n<number> is an optional parameter")
+  embedVar.add_field(name="Spam",value="Use `y.spam <user> <message> <number>` \nSpams a mentioned user the specified number of times. \n<number> is an optional parameter\nIf you want the message to have more than one word, use quotes")
+  embedVar.add_field(name="Spam DM",value="Use `y.spamdm <user> <message> <number>` \nSpams a mentioned user in their DM the specified number of times. Use this when you dont want to spam on the server \n<number> is an optional parameter\nIf you want the message to have more than one word, use quotes")
   embedVar.add_field(name="Hello There",value="Replies with General Kenobi",inline=False)
   embedVar.add_field(name="Salam",value="Replies with Alaikum Assalam",inline=False)
   embedVar.add_field(name="F",value="Replies with F to pay  respects",inline=False)
   embedVar.set_footer(text="PS: If you are a Mod, change the **@Yemmerald** role's color to `#26a43b` for __*~aEsThEtIcS~*__.")
   await ctx.reply(embed=embedVar)
 
+@client.command(name='poll')
+async def poll(ctx,*args):
+  response=f'<@everyone>'
+  for i in args:
+    response+=f"\n{i}"
+  await ctx.send(response)
+  
 @client.command(name='spam')
-async def spam(ctx,user:discord.User,message="", num=1):
+async def spam(ctx,user:discord.User,message, num=1):
+  if num>0:
+    num=20
   for i in range(int(num)):
     await ctx.send(f"{user.mention} {message}")
 
 @client.command(name='spamdm')
 async def spamdm(ctx,user: discord.User, message, num=1):
+  if num>20:
+    num=20
   for i in range(int(num)):
     await user.send(f"{user.mention} {message}")
 
